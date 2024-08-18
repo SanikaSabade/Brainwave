5b2a2e80950133e77eca37b1828187308761461d
SanikaSabade <sanikas139@gmail.com>
2024-08-18 21:45:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+
+const App =() => {
+  
+    return (
+      <>
+      
+      <div className="pt-[8.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header/>
+        <Hero/>
+
+      </div>
+      <ButtonGradient />
+      </>
+    );
+  };
+
+export default App;
