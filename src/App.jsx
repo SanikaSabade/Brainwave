a85b400dd8ef7e52728927688ae24240d41df116
Sanika Sabade <sanikas139@gmail.com>
2024-09-01 20:24:24 +0530

    completed website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
+import Benifits from './Components/Benifits';
+import Collaboration from './Components/Collaboration';
+import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Hero from './Components/Hero';
+import Pricing from './Components/Pricing';
+import Roadmap from './Components/Roadmap';
+import Services from './Components/Services';
+
 
 const App =() => {
   
@@ -10,6 +17,13 @@ const App =() => {
       <div className="pt-[8.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header/>
         <Hero/>
+        <Benifits/>
+        <Collaboration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
+
 
       </div>
       <ButtonGradient />
